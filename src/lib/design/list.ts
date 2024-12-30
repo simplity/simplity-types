3e7f9e267ef5466eeffdb064e52c50bb90728290
Raghu Bhandi <raghu.bhandi@gmail.com>
2024-12-31 02:40:18 +0530

    renamed some attributes for better readability

--- src/lib/design/list.ts
+++ src/lib/design/list.ts
@@ -41,16 +41,43 @@ export type FixedKeyedList = {
 export type RuntimeList = {
   name: string;
   listType: 'runtime';
-  isKeyed: boolean;
-  keyIsNumeric?: boolean;
-  valueIsNumeric?: boolean;
-  okToCache?: boolean;
+  /**
+   * note that this is db-table-name and not record-name
+   */
   dbTableName: string;
+  /**
+   * name of the db-column for the internal value
+   */
   dbColumn1: string;
+  /**
+   * true if dbColumn1 (internal value) is numeric
+   */
+  column1IsNumeric?: boolean;
+  /**
+   * name of teh db-column for the display value
+   */
   dbColumn2: string;
+  /**
+   * true if this list is based on its parent key. Like states for a country.
+   * If true, then keyColumn must be specified
+   */
+  isKeyed: boolean;
+  /**
+   * name of the column for the key-field. like country_id for list of states for a country
+   */
   keyColumn?: string;
+  /**
+   * true if the parent key is numeric
+   */
+  keyIsNumeric?: boolean;
+
+  okToCache?: boolean;
   tenantColumn?: string;
   activeColumn?: string;
+  /**
+   * used if getAllEntries is to be used.
+   * For example, the client can request all states for all countries and cache them once
+   */
   parentIdColumnName?: string;
   parentNameColumnName?: string;
 };
